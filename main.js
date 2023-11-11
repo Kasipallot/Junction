74dea91af87942ff3205fb7dcc48737543c30e55
Samuli Öhman <samuli.ohman00@gmail.com>
2023-11-11 13:56:18 +0200

    Added background, fixed scaling, improved camera.

--- main.js
+++ main.js
@@ -5,17 +5,18 @@ import { Inspector } from '@babylonjs/inspector';
 const canvas = document.getElementById('renderCanvas');
 
 const engine = new BABYLON.Engine(canvas);
+let slotMachineMesh; // Declare it in a higher scope
 
 const createScene = async function () {
   const scene = new BABYLON.Scene(engine);
 
   // This creates and positions a free camera (non-mesh)
   const camera = new BABYLON.ArcRotateCamera(
-    "my first camera",
+    "my camera",
     0,
     -1,
-    5,
-    new BABYLON.Vector3(-7, 0, 0),
+    45,
+    new BABYLON.Vector3(0, 0, 0),
     scene
   );
   camera.attachControl(canvas, true);
@@ -24,8 +25,8 @@ const createScene = async function () {
   const utilLayer = new BABYLON.UtilityLayerRenderer(scene);
 
   const ground = new BABYLON.MeshBuilder.CreateGround('', {
-    height: 5,
-    width: 10,
+    height: 100,
+    width: 200,
     subdivisions: 5,
     subdivisionsX: 10
   });
@@ -40,8 +41,16 @@ const createScene = async function () {
   // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 0.7;
 
-  const slotMachine = await BABYLON.SceneLoader.ImportMeshAsync("", "/assets/", "crazyslots.glb", scene);
-  const slotMachineMesh = slotMachine.meshes[0];
+  const slotMachine = await BABYLON.SceneLoader.ImportMeshAsync("", "/assets/", "crazyslots1.glb", scene);
+  const casino = await BABYLON.SceneLoader.ImportMeshAsync("", "/assets/", "blacklodge1.glb", scene);
+
+  slotMachineMesh = slotMachine.meshes[0];
+  slotMachineMesh.scaling.scaleInPlace(2.8);
+
+  const casinoMesh = casino.meshes[0];
+  casinoMesh.rotate(new BABYLON.Vector3(0, 1, 0), Math.PI * 3 / 4);
+  casinoMesh.position.x = -49;
+  casinoMesh.position.z = -12;
 
   ground.receiveShadows = true;
 
@@ -54,14 +63,15 @@ const createScene = async function () {
     }
   }
   camera.setTarget(slotMachineMesh.position.add(new BABYLON.Vector3(0, 3, 0)));
-
-
   return scene;
 }
 
 const scene = await createScene();
 
 engine.runRenderLoop(function () {
+  if (slotMachineMesh) {
+    //slotMachineMesh.rotate(new BABYLON.Vector3(0, 1, 0), 0.01)
+  }
   scene.render();
 });
 
